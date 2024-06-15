c8a263f1cae539eb044df2112583268a3c3d6261
william <santifon@gmail.com>
2024-06-14 20:33:22 -0500

    Improve not found component

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+
+  ngOnInit(): void {
+    const visual = document.getElementById("backfont")!;
+    const events = ['resize', 'load']
+
+    events.forEach(function (e) {
+      window.addEventListener(e, function () {
+        const width = window.innerWidth
+        const height = window.innerHeight
+        const ratio = 30 / (width / height)
+        visual.style.transform = "translate(-50%, -50%) rotate(-" + ratio + "deg)"
+      });
+    });
+  }
 
 }
